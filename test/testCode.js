ca3677de4d3d76caf242aeb14a725566b686ef4a
Jeremy Boateng <jeremyba@spartaglobal.co>
2017-02-16 11:14:42 +0000

    fixed issues with test code

--- test/testCode.js
+++ test/testCode.js
@@ -1,7 +1,7 @@
 // image url: http://vignette3.wikia.nocookie.net/simpsons/images/b/b3/Duff.png/revision/latest?cb=20100807123918
 var chai = require('chai');
 var chaiHttp = require('chai-http');
-var app = require('app');
+var app = require('../app');
 var should = chai.should();
 var expect = require('chai').expect;
 var Beer = require('../models/beer');
@@ -10,11 +10,11 @@ chai.use(chaiHttp);
 
 describe('Beers', function (){
 	var beer = new Beer({
-		image_url: "http://vignette3.wikia.nocookie.net/simpsons/images/b/b3/Duff.png/revision/latest?cb=20100807123918"
+		image_url: "http://vignette3.wikia.nocookie.net/simpsons/images/b/b3/Duff.png/revision/latest?cb=20100807123918",
 		name: "Duff Beer",
 		tagline: "Can't get enough of that wonderful Duff!",
 		abv: 6.4,
-		description: "The quality of Duff appears to be standard for mass quantity supply beer. Strict regulations are supposedly maintained at Quality Assurance."
+		description: "The quality of Duff appears to be standard for mass quantity supply beer. Strict regulations are supposedly maintained at Quality Assurance.",
 		food_pairing: ["Donuts","More Duff"]
 	})
 
@@ -22,7 +22,7 @@ describe('Beers', function (){
 		beer.save(function(err, newBeer) {
 	      if (err) return console.log(err);
 	      console.log("made newBeer with id " + newBeer.id);
-	      beer.id = newbeer.id;
+	      beer.id = newBeer.id;
 	  })
 	})
 
