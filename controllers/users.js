65fc3d0228199f6564ff0be879eb775f43ccb2ba
Jeremy Boateng <jeremyba@spartaglobal.co>
2017-02-17 09:57:37 +0000

    added createUser to users controller

--- controllers/users.js
+++ controllers/users.js
@@ -4,3 +4,21 @@ function newUser(req,res) {
 
   res.render('users/new' , {title:"Register"});
 }
+
+// CREATE - Handles registrations
+function createUser(req,res){
+
+  // save the user
+  var user = new User(req.body);
+
+  user.save(function(err,user){
+
+    // check for errors and return 500 if there was a problem
+    if(err) req.flash('error' , err.message);
+
+    // redirect to the posts index page
+    res.redirect("/");
+
+  });
+
+}
